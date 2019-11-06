940086601627fc09bf8b9bfdf2ba397429faff97
goerch <jhr.walter@googlemail.com>
2019-11-06 17:37:13 +0100

    Tests for issue #29

--- stability_selection/tests/test_stability_selection.py
+++ stability_selection/tests/test_stability_selection.py
@@ -4,7 +4,10 @@ import numpy as np
 from numpy.testing import assert_almost_equal
 from nose.tools import raises
 
-from sklearn.linear_model import Lasso
+from scipy import sparse
+
+from sklearn.feature_selection import SelectPercentile
+from sklearn.linear_model import Lasso, LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.utils import check_random_state
@@ -12,7 +15,7 @@ from stability_selection import StabilitySelection, plot_stability_path
 
 
 def _generate_dummy_regression_data(p=1000, n=1000, k=5,
-                                    random_state=123321):
+                                    random_state=123321, csr=False):
     rng = check_random_state(random_state)
 
     X = rng.normal(loc=0.0, scale=1.0, size=(n, p))
@@ -22,11 +25,11 @@ def _generate_dummy_regression_data(p=1000, n=1000, k=5,
 
     y = np.matmul(X, betas)
 
-    return X, y, important_betas
+    return X if not csr else sparse.csr_matrix(X), y, important_betas
 
 
 def _generate_dummy_classification_data(p=1000, n=1000, k=5,
-                                        random_state=123321):
+                                        random_state=123321, csr=False):
 
     rng = check_random_state(random_state)
 
@@ -38,13 +41,52 @@ def _generate_dummy_classification_data(p=1000, n=1000, k=5,
     probs = 1 / (1 + np.exp(-1 * np.matmul(X, betas)))
     y = (probs > 0.5).astype(int)
 
-    return X, y, important_betas
+    return X if not csr else sparse.csr_matrix(X), y, important_betas
 
 
 def test_stability_selection_classification():
     n, p, k = 1000, 1000, 5
 
     X, y, important_betas = _generate_dummy_classification_data(n=n, k=k)
+    selector = StabilitySelection(lambda_grid=np.logspace(-5, -1, 25), verbose=1)
+    selector.fit(X, y)
+
+    chosen_betas = selector.get_support(indices=True)
+    X_r = selector.transform(X)
+
+    assert_almost_equal(important_betas, chosen_betas)
+    assert(X_r.shape == (n, k))
+    assert(selector.stability_scores_.shape == (p, selector.lambda_grid.shape[0]))
+
+
+def test_stability_selection_classification_with_selection():
+    n, p, k = 1000, 1000, 5
+
+    X, y, important_betas = _generate_dummy_classification_data(n=n, k=k)
+
+    base_estimator = Pipeline([
+        ('selection', SelectPercentile()),
+        ('model', LogisticRegression(penalty='l1'))
+    ])
+
+    selector = StabilitySelection(base_estimator=base_estimator,
+                                  lambda_name='model__C',
+                                  lambda_grid=np.logspace(-5, -1, 25),
+                                  verbose=1)
+    selector.fit(X, y)
+
+    chosen_betas = selector.get_support(indices=True)
+    X_r = selector.transform(X)
+
+    assert_almost_equal(important_betas, chosen_betas)
+    assert (X_r.shape == (n, k))
+    assert (selector.stability_scores_.shape == (p, selector.lambda_grid.shape[0]))
+
+
+def test_stability_selection_sparse_classification():
+    n, p, k = 1000, 1000, 5
+
+    X, y, important_betas = _generate_dummy_classification_data(n=n, k=k, csr=True)
     selector = StabilitySelection(lambda_grid=np.logspace(-5, -1, 25), verbose=1)
     selector.fit(X, y)
 
@@ -63,6 +105,28 @@ def test_stability_selection_regression():
 
     base_estimator = Pipeline([
         ('scaler', StandardScaler()),
+        ('model', Lasso())
+    ])
+
+    lambdas_grid = np.logspace(-1, 1, num=10)
+
+    selector = StabilitySelection(base_estimator=base_estimator,
+                                  lambda_name='model__alpha',
+                                  lambda_grid=lambdas_grid)
+    selector.fit(X, y)
+
+    chosen_betas = selector.get_support(indices=True)
+
+    assert_almost_equal(important_betas, chosen_betas)
+
+
+def test_stability_selection_sparse_regression():
+    n, p, k = 500, 1000, 5
+
+    X, y, important_betas = _generate_dummy_regression_data(n=n, k=k, csr=True)
+
+    base_estimator = Pipeline([
+        ('scaler', StandardScaler(with_mean=False)),
         ('model', Lasso())
     ])
 
